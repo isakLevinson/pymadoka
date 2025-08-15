88514485b608c4f8840c4d48decf41e94c83ee18
Isak Levinson <isak.levinson@gmail.com>
2025-08-16 00:34:47 +0300

    bleak 1.0.1

--- pymadoka/connection.py
+++ pymadoka/connection.py
@@ -5,15 +5,10 @@ from asyncio.exceptions import CancelledError
 from asyncio.futures import Future
 import logging                                    
 
- 
 import subprocess
 import sys
 from enum import Enum
-
-
 from bleak import BleakClient, BleakScanner
-from bleak.discovery import discover
-
 
 from typing import Dict
 
